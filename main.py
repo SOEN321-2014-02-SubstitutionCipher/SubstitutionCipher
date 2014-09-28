8ea8339f81b652b7c606b0e6c60742c0815466a6
Norman Mirotchnick <norman@redhot5.com>
2014-09-28 12:47:15 -0400

    -- main.py changes
    -- removed local IDE files

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-__author__ = 'Norman Mirotchnick'
+__author__ = 'SOEN321-2014-02 Group: Connor Bode, Sid the Banzai Master, Eric Bozikian, Norman Mirotchnick'
+"""
+date-created: 2014-09-28
+last-update:  2014-09-28
+version:  0.0.1
+IDE: PhyCharm 3.4.1
+"""
+
